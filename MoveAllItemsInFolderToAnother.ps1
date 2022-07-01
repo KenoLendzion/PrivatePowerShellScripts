ff3f226c6e91a68ae80b924c16861cd71eebc4b9
kenolendzion <kenolendzion@gmail.com>
2022-07-01 22:30:47 +0200

    MoveAllItemsInFOlderToAnother Initial

---
+++ MoveAllItemsInFolderToAnother.ps1
@@ -0,0 +1,14 @@
+﻿param($Source = "C:\Users\kenol\Desktop", $Destination = "C:\Users\kenol\Documents\ItemsFromDesktop")
+
+if(-not(Test-Path -Path $Source) -or $Source -eq $null)
+{
+    throw "The source folder doen't exist or is null."
+}
+
+if (-not(Test-Path -Path $Destination) -or ($Destination -eq $null))
+{
+    mkdir -Path $Destination
+}
+
+Get-ChildItem $Source | Move-Item -Destination $Destination -ErrorAction Inquire
+
